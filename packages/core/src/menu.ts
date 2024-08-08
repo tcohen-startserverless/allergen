7b4bae8688e79e7f9214026ffc4f380457bf9fb6
tcohen-startserverless <trevor.cohen@startserverless.dev>
2024-08-07 21:51:45 -0700

    ..

--- packages/core/src/menu.ts
+++ packages/core/src/menu.ts
@@ -1,7 +1,3 @@
-// import { Resource } from "sst";
-// import { bus } from "sst/aws/bus";
-// import { Restaurant } from "./schemas";
-// import { RestaurantEntity } from "./entities/restaurant";
 import { scrapeUrl } from "./scrape";
 import { MenuEntity } from "./entities/menu";
 import type { RestaurantType } from "./schemas";
@@ -9,13 +5,12 @@ import type { RestaurantType } from "./schemas";
 export const addMenuData = async (data: RestaurantType["Created"]) => {
   const url = `https://${data.domain}${data.page}`;
   const response = await MenuEntity.get(data).go();
-  // console.log({ response.data });
   if (response.data) {
     console.log("returning");
     return response.data;
   }
   const scrapedData = await scrapeUrl({ url });
-  console.log("saving to menu");
+  console.log("saving menu");
   const record = await MenuEntity.upsert({
     ...data,
     data: JSON.stringify(scrapedData),
--- src/pages/restaurants.json.ts
+++ src/pages/restaurants.json.ts
@@ -6,32 +6,17 @@ import type { AppType } from "@functions/api";
 const api = Resource.Api.url;
 const client = hc<AppType>(api);
 
-// export async function GET({ params }) {
-//   const id = params.id;
-//   const product = await getProduct(id);
-//   if (!product) {
-//     return new Response(null, {
-//       status: 404,
-//       statusText: "Not found",
-//     });
-//   }
-//   return new Response(JSON.stringify(product), {
-//     status: 200,
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-// }
-
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
   const url = data.get("url");
   if (!url) return new Response("Missing url", { status: 400 });
-  const site = await client.site.create.$post({
+  const res = await client.site.create.$post({
     form: {
       url,
     },
   });
+  const site = await res.json();
+  console.log({ site });
   // return new redirect();
   return new Response(JSON.stringify(site));
 };
